88fde2c96cab955bbb0bfe9b343643ea815bb3b5
stilxam <m.l.h.litsios@student.tue.nl>
2023-05-16 10:52:14 +0200

    update

--- sqlMaker.py
+++ sqlMaker.py
@@ -18,44 +18,33 @@ def main():
     lsoa = loadBarnetLSOA()
     imd_by_lsoa_15 = get_ranking_IMD_by_LSOA(2015)
     filt15 = filter(imd_by_lsoa_15, lsoa)
+    
     imd_by_lsoa_19 = get_ranking_IMD_by_LSOA(2019)
     filt19 = filter(imd_by_lsoa_19, lsoa)
+
     conced = pd.concat([filt15["LSOA Code"], filt19["LSOA Code"]])
     conced = pd.concat([conced, lsoa])
     un = conced.drop_duplicates()
     
     conn = sqlite3.connect(dbPath())
-    fin = []
+    cursor = conn.cursor()
+    cursor.execute("DROP TABLE IF EXISTS crimes_similar_LSOA")
+
 
     for monthDir in tqdm(listFilesInDirectory(crime_uk_per_month_dir())):
         df = []
         for region in listFilesInDirectory(monthDir):
             reg = pd.read_csv(region)
-            # print(reg.info())
-            df.append(reg
-                      # [
-                # ["Month", 
-                 # "Reported by", 
-                 # "Falls within", 
-                 # "Longitude", 
-                 # "Latitude", 
-                 # "Location", 
-                 # "LSOA code", 
-                 # "LSOA name", 
-                 # "Crime type"
-                 # ]
-                # ]
-                      )
+            df.append(reg)
+        
         conced = pd.concat(df, ignore_index=True)
         filtered = conced[conced["LSOA code"].isin(un)]
-        filtered.drop(["Context", "Last outcome category", "Crime ID"], axis=1, inplace=True)
-        # print(filtered.info() )
-        fin.append(filtered)
-        # filtered.to_sql('crimes_similar_LSOA', conn, if_exists='append', index=False)
-         
-    finalDF = pd.concat(fin, ignore_index=True)
-    print(finalDF.info())
-    finalDF.to_sql("crimes_similar_LSOA", conn, if_exists="append", index=False)
+        final = filtered[["Month", "Reported by", "Falls within", "Longitude", "Latitude", "Location", "Crime type"]]
+        # print(final.info() )
+        
+        final.to_sql("crimes_similar_LSOA", conn, if_exists="append", index=False)
+        
+
     conn.close()
 
 if __name__ == "__main__":
